566816b2ec83299fef23d9f3fcf84a260df7eafa
Felipe Juchem <felipe_build@hotmail.com>
2021-02-25 16:55:46 -0300

    backup

--- OnboardingSIGDB1.API/Startup.cs
+++ OnboardingSIGDB1.API/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using OnboardingSIGDB1.Data;
 using System;
 using System.Collections.Generic;
@@ -13,13 +16,19 @@ namespace OnboardingSIGDB1.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(
                 options => options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=OnBoarding"));
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,16 +39,7 @@ namespace OnboardingSIGDB1.API
                 app.UseDeveloperExceptionPage();
             }
 
-            
-
-
-
-
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
--- OnboardingSIGDB1.Data/DataContext.cs
+++ OnboardingSIGDB1.Data/DataContext.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using OnboardingSIGDB1.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OnboardingSIGDB1.Data
 {
-    class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite(@"Server=(localdb)\mssqllocaldb;Database=Test");
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var empresa = modelBuilder.Entity<Empresa>();
+
+            empresa.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            empresa.Property(tc => tc.Id)
+                 .IsRequired();
+
+
+            empresa.Property(tc => tc.Cnpj)
+                 .IsRequired();
+
+            empresa.Property(tc => tc.DataFundacao)
+                 .IsRequired();
+
+
+
+
+
+
+
+
+
+        }
+
+
     }
 }
